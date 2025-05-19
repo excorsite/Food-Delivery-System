d0a136e7a6c08c67d6b31bd1a7cbc76c60bfe4a5
Excorsite <panditsuyog4@gmail.com>
2025-05-19 19:56:03 +0545

    Home page

--- my-vintage-delivery/src/components/Home.jsx
+++ my-vintage-delivery/src/components/Home.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 function Home() {
+  const [variable, setVariable] = useState();
+
+  function handleChange(event) {
+    setVariable(event.target.value);
+  }
   return (
     <>
       <div className="resturant-home">
         <h1>THis is a home page</h1>
+        <input type="text" value={variable} />
+        <button onChange={handleChange}>Click me </button>
       </div>
     </>
   );
